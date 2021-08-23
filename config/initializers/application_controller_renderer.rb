13230b189dc08daa04cf6a878790b7dedd2cbd60
myhearisranch <myhearisranch@gmail.com>
2021-08-23 12:08:51 +0000

    画像投稿機能を作ろう

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'f635affb6aa028f248bafa2dc83aafba143a7d399e0e677153b8488aa430248e035705e4ac3f4722b339a3f2f5d14e0cba4468d43c5ecb5ebc31c80f96247b8a'
